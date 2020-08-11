b4e6a94ac8799a00a77c736b7c353e006f396d16
Jonas Lindemann <jonas.lindemann@lunarc.lu.se>
2020-08-11 15:35:06 +0200

    Initial commit

---
+++ templates/wizards/projects/plainfor/main.f90
@@ -0,0 +1,23 @@
+program myfortran
+
+    ! --- Put use statements here
+    !
+    ! use mymodule
+
+    implicit none
+
+    ! --- Declare your variables here
+    !
+    ! integer :: a
+
+    print*, 'Hello, Fortran!'
+
+contains
+
+    ! --- Add your subroutines and functions here
+    ! 
+    ! subroutine mysub
+    ! 
+    ! end subroutine mysub
+
+end program myfortran
